2c87baae9caf21434d4e27c113d208bed2ded412
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2023-11-07 08:03:23 -0600

    start the table

--- main.c
+++ main.c
@@ -12,19 +12,26 @@ int main()
 
     printf("ingrese dos valores de tipo caracter en forma x y: \n");
     scanf("%c %c", &c1, &c2);
-    //printf("ingrese dos valores de tipo entero en forma x y: \n");
-    //scanf("%d %d", &n1, &n2);
-    //printf("ingrese dos valores de tipo flotante en forma x y: \n");
-    //scanf("%f %f", &f1, &f2);
+    printf("ingrese dos valores de tipo entero en forma x y: \n");
+    scanf("%d %d", &n1, &n2);
+    printf("ingrese dos valores de tipo flotante en forma x y: \n");
+    scanf("%f %f", &f1, &f2);
 
     ptr_c = &c1;
     ptr_n = &n1;
     ptr_f = &f1;
 
-    printf("direccion de variable: %c\n", &c1);
-    printf("direccion de apuntador: %c\n", &ptr_c);
-    printf("coneniudo de auntador: %c\n", ptr_c);
-    printf("contenido indirecto: %c\n", *ptr_c);
+    //variable       | direccion de aountador       | conteniodo del apuntador       
+    printf("variable\t| direccion de aountador\t| contenido del apuntador\t| contenido indirecto\t\n");
+    printf("==============================================================================================================================================================");
+    printf("c1 \t| %p \t| %p \t| %p \t| %c \t|", &c1, &ptr_c, ptr_c, *ptr_c);
+    printf("n1 \t| %p \t| %p \t| %p \t| %d \t|", &n1, &ptr_n, ptr_n, *ptr_n);
+    printf("f1 \t| %p \t| %p \t| %p \t| %0.2f \t|", &f1, &ptr_f, ptr_f, *ptr_f);
+    
+
+    ptr_c = &c2;
+    ptr_n = &n2;
+    ptr_f = &f2;
 
     return 0;
 }
